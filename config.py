d456b97e8e65d652557e1d88ffe4582db78c03b7
Patrick Ingham <pingham1991@gmail.com>
2023-10-23 15:49:06 -0700

    Adding basic configuration info

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Flask configuration file, referenced in the `create_app` code from __init__
+ENVIRONMENT = 'development'
+FLASK_APP = 'cube-rubrics'
+SECRET_KEY = 'notsosecret1'
+
+if __name__ == '__main__':
+    print('Config file for flask rprime app')
+
